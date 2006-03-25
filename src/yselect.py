9358bce281fc43a1c19be0c23c775372dedfdbd2
Devan Goodwin <dgoodwin@dangerouslyinc.com>
2006-03-25 01:41:50 +0000

    First cut at a MainApplication class, hooked up UP / DOWN arrow keys on the main menu.

--- src/yselect.py
+++ src/yselect.py
@@ -19,11 +19,11 @@ class MainMenu:
     """
     yselect main menu screen.
     """
-    
+
     def __init__(self):
         self.title = \
             "RPM/Yum %s package handling frontend." % (program_name)
-        
+
         self.entries = (
             ("update", "[U]pdate",
                 "Update list of available packages, if possible."),
@@ -32,6 +32,8 @@ class MainMenu:
             ("install", "[I]nstall", "Install and upgrade wanted packages."),
             ("quit", "[Q]uit", "Quit %s." % (program_name))
         )
+        # Start off with the "update" menu item selected:
+        self.selectedEntry = 0
 
         self.navigation_info = \
             "Move around with ^P and ^N, cursor keys, initial letters," + \
@@ -43,46 +45,51 @@ class MainMenu:
             "This is free software; see the GNU General Public Licence " + \
             "version 2\n" + \
             "or later for copying conditions.  There is NO warrenty.  See\n" + \
-            "%s --licence for details." 
-            
+            "%s --licence for details.\n"
+
         self.copyright = self.copyright % (version, program_name)
-      
-        self.selected = "update"
-      
+
     def move_up(self):
-        pass
+        """
+        Move the menu cursor up one entry, if we're not already at the top of the list.
+        """
+        if self.selectedEntry > 0:
+            self.selectedEntry = self.selectedEntry - 1
 
     def move_down(self):
-        pass
+        """
+        Move the menu cursor down an entry, if we're not already at the bottom of the list.
+        """
+        if self.selectedEntry < len(self.entries) - 1:
+            self.selectedEntry = self.selectedEntry + 1
 
     def select(self):
         pass
 
     def paint(self, window):
         window.addstr(self.title, curses.A_BOLD)
-        
+
         x_pos = 2
         for i in range(len(self.entries)):
             menu_entry = self.entries[i]
 
-            if self.selected == menu_entry[0]:
+            if self.selectedEntry == i:
                 prefix = " * "
                 format = curses.A_REVERSE
             else:
                 prefix = "   "
                 format = curses.A_NORMAL
-            
+
             entry_string = \
                 prefix + str(i) + ". " + menu_entry[1] + "\t" + menu_entry[2]
             window.addstr(x_pos, 0, entry_string, format)
             x_pos = x_pos + 1
-     
+
         x_pos = x_pos + 1
         window.addstr(x_pos, 0, self.navigation_info, curses.A_BOLD)
-        
+
         x_pos = x_pos + 3 # The previous string was two lines
         window.addstr(x_pos, 0, self.copyright)
-
 
 def initialize_curses():
     """ Start up the curses UI. """
@@ -92,11 +99,11 @@ def initialize_curses():
     curses.cbreak()
 
     stdscr.keypad(True)
-    
+
     return stdscr
 
 def terminate_curses(stdscr):
-    """ 
+    """
     Shut down the curses UI. We set the console back to a nice condition.
     """
     stdscr.keypad(False)
@@ -105,22 +112,28 @@ def terminate_curses(stdscr):
     curses.echo()
     curses.endwin()
 
-def main():
-    """ Program main entry point. """
-    stdscr = initialize_curses()
+class MainApplication:
+    def __init__(self):
+        stdscr = initialize_curses()
 
-    menu = MainMenu()
+        menu = MainMenu()
 
-    menu.paint(stdscr)
-    stdscr.refresh()
+        menu.paint(stdscr)
+        stdscr.refresh()
 
-    while True:
-        char = stdscr.getch()
+        while True:
+            char = stdscr.getch()
 
-        if char == ord('q'):
-            break
+            if char == ord('q'):
+                break
+            elif char == curses.KEY_UP:
+                menu.move_up()
+                menu.paint(stdscr)
+            elif char == curses.KEY_DOWN:
+                menu.move_down()
+                menu.paint(stdscr)
 
-    terminate_curses(stdscr)
+        terminate_curses(stdscr)
 
 if __name__ == "__main__":
-    main()
+    yselect = MainApplication()
