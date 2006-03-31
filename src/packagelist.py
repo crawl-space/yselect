b3a3584ee0b655eb8c7464cea955f30066d16bf7
James Bowes <jbowes@dangerouslyinc.com>
2006-03-31 00:57:13 +0000

    Fix up rendering background colors for list thingy

--- src/packagelist.py
+++ src/packagelist.py
@@ -5,7 +5,7 @@ class PackageView:
 
     def __init__(self, window):
         self.window = window
-        self.window.attrset(curses.color_pair(1))
+        self.window.bkgd(" ", curses.color_pair(1))
         
         (height, width) = self.window.getmaxyx()
 
@@ -16,6 +16,8 @@ class PackageView:
     def update(self):
         self.list_window.update()
         self.details_window.update()
+
+        (height, width) = self.window.getmaxyx()
 
         self.window.addstr(0, 0, "yselect - inspection of package states (avail., priority)")
         self.window.refresh()
@@ -34,12 +36,13 @@ class ListView:
 
     def __init__(self, window):
         self.window = window
-        self.window.attrset(curses.color_pair(2))
+        self.window.bkgd(" ", curses.color_pair(2))
         padwin = self.window.derwin(1,0)
         self.pad = padwin
-        self.pad.attrset(curses.color_pair(0))
+        self.pad.bkgd(" ", curses.color_pair(0))
 
     def update(self):
+        (height, width) = self.window.getmaxyx()
         self.window.addstr(0,0, "EIOM Pri Section\tPackage\tInst.ver\tAvail.ver\tDescription")
         self.pad.addstr(0,3, "--- sublist ---")
         self.window.refresh()
@@ -48,15 +51,15 @@ class DetailsView:
 
     def __init__(self, window):
         self.window = window
-        self.window.attrset(curses.color_pair(2))
+        self.window.bkgd(" ", curses.color_pair(2))
         (height, width) = self.window.getmaxyx()
         self.details_pad = self.window.derwin(height - 2, 0, 1, 0) 
-        self.details_pad.attrset(curses.color_pair(0))
+        self.details_pad.bkgd(" ", curses.color_pair(0))
 
     def update(self):
         (height, width) = self.window.getmaxyx()
-        self.window.addnstr(0,0, "bar!   Required" + " " * width, width)
-        self.window.addnstr(height - 1, 0, "press d for more." + " " * width, width - 1)
+        self.window.addstr(0,0, "bar!   Required")
+        self.window.addstr(height - 1, 0, "press d for more.")
         self.details_pad.addstr(0,0, "DETAILS")
 
 
