590acb0f1c467d1306d0f5eb1f9ee4632a65bcf7
James Bowes <jbowes@dangerouslyinc.com>
2006-04-08 01:45:03 +0000

    Add package list displaying

--- src/packagelist.py
+++ src/packagelist.py
@@ -9,7 +9,10 @@ class PackageView:
         
         (height, width) = self.window.getmaxyx()
 
-        self.list_window = ListView(window.derwin(1, 0))
+        list_model = ListModel()
+        list_model.addSubList(ListModel())
+
+        self.list_window = ListView(window.derwin(1, 0), list_model)
         self.details_window = DetailsView(window.derwin(height / 2, 0))
         self.details_window.setModel(DetailsModel())
 
@@ -35,20 +38,29 @@ class PackageView:
 
 class ListView:
 
-    def __init__(self, window):
+    def __init__(self, window, list_model):
         self.window = window
         self.window.bkgd(" ", curses.color_pair(2))
         padwin = self.window.derwin(1,0)
         self.pad = padwin
         self.pad.bkgd(" ", curses.color_pair(0))
 
+        self.list_model = list_model
+
     def update(self):
         (height, width) = self.window.getmaxyx()
         self.window.addstr(0,0, "EIOM Pri Section\tPackage\tInst.ver\tAvail.ver\tDescription")
-        self.add_menu_title(0, 5, "another sublist", 1)
-        self.add_menu_title(1, 5, "another sublist", 2)
-        self.add_menu_title(2, 5, "another sublist", 3)
+        self.add_list(self.list_model, 0, 1)
         self.window.refresh()
+
+    def add_list(self, list_model, row, depth):
+        self.add_menu_title(row, 5, list_model.title, depth)
+        for i in range(len(list_model.packages)):
+            entry = list_model.packages[i]
+            if entry.__class__ == ListModel:
+                self.add_list(entry, i + row + 1, 2)
+            else:
+                self.pad.addstr(i + row + 1, 0, list_model.packages[i])
 
     def add_menu_title(self, y, x, title, depth):
         """
@@ -63,6 +75,16 @@ class ListView:
         self.pad.addstr(y, x + line_length + 1 , title, curses.A_BOLD)
         self.pad.hline(y, x + line_length + 2 + len(title), curses.ACS_HLINE,
             line_length)
+
+
+class ListModel:
+
+    def __init__(self):
+        self.title = "All Packages"
+        self.packages = ['pkg1', 'pkg2', 'pkgz', 'pkg5']
+
+    def addSubList(self, sub_list):
+        self.packages.append(sub_list)
 
 
 class DetailsView:
